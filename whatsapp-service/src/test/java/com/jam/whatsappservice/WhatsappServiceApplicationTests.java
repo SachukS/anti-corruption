9e1b6c21a0d7ce7d9dd36e6b64bc368431d224a4
Oleksandr Sachuk <sachuk29sanya@gmail.com>
2024-10-31 21:11:16 +0200

    Initial commit

---
+++ whatsapp-service/src/test/java/com/jam/whatsappservice/WhatsappServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jam.whatsappservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
